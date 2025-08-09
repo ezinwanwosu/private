3bcdfdf7ef1e521df62e94c0951cba8b2f05e524
Ezinwa <bianca.e.nwosu@gmail.com>
2025-08-09 22:40:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,8 @@ def dashboard():
     print('rendering....')
     return render_template('dashboard.html')
 if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port, debug=True)
+
